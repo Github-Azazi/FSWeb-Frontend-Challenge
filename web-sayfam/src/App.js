9db1d73daf115dac42a10d5c70604b1ceb94de6c
Github-Azazi <cs50_scratch@yahoo.com>
2024-02-12 14:06:45 +0300

    componentler ve context tanımlandı

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -1,70 +1,62 @@
 
 import './App.css';
-
 import React, { useState } from 'react'
+import { useContext } from "react";
+import { CiteContext } from "./contexts/CiteContext";
+// import Hero from "./components/Hero";
+// import Skills from "./components/Skills";
+// import Profile from "./components/Profile";
+// import Projects from "./components/Projects";
+// import Footer from "./components/Footer";
+// import ModeSwitch from "./components/ModeSwitch";
+// import Header from "./components/Header";
+// import {ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+const initialDAta = {
+  name: "Ozan Azazi",
+  birthday: "01.11.1975",
+  cityofresidence: "Antakya",
+  education: "Çukurova Üniversitesi İşletme Lisans,2002",
+  preference: "Frontend, UI",
+  mail: "cs50_scratch@yahoo.com",
+};
 
 function App() {
-  
-  const [darkMode, setDarkMode] = useState(false);
-
-
-  const toggleDarkMode = () => {
-
+    const [darkMode, setDarkMode] = useState(false);
+    const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
-
-
   const [language, setLanguage] = useState("tr");
-
-
   const changeLanguage = (lang) => {
-   
-    setLanguage(lang);
+       setLanguage(lang);
   };
-
- 
+  const [profildata, setProfildata] = useState(initialDAta);
+  const { theme } = useContext(CiteContext);
   return (
-   
-    <div className={darkMode ? "bg-gray-900" : "bg-gray-100"}>
-   
-      <button
-      
-        onClick={toggleDarkMode}
-
+       <div className={darkMode ? "bg-gray-900" : "bg-gray-100"}>
+         <button
+          onClick={toggleDarkMode}
         className="p-4 m-4 rounded-lg bg-blue-500 text-white"
       >
-       
-        {darkMode ? "Light Mode" : "Dark Mode"}
+       {darkMode ? "Light Mode" : "Dark Mode"}
       </button>
-     
       <button
-
         onClick={() => changeLanguage("tr")}
-     
         className="p-4 m-4 rounded-lg bg-red-500 text-white"
       >
-   
-        TR
+           TR
       </button>
       <button
-       
         onClick={() => changeLanguage("en")}
-
         className="p-4 m-4 rounded-lg bg-green-500 text-white"
       >
-     
         EN
       </button>
-   
       <h1 className={darkMode ? "text-white" : "text-black"}>
-     
-        {language === "tr" ? "Kişisel Web Sayfam" : "My Personal Web Page"}
+      {language === "tr" ? "Kişisel Web Sayfam" : "My Personal Web Page"}
       </h1>
-   
-    </div>
+      </div>
   );
 }
-
-
 export default App;
--- web-sayfam/src/components/Header.js
+++ web-sayfam/src/components/Header.js
@@ -1,1 +1,44 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+import { useContext } from "react";
+import { CiteContext } from "./contexts/CiteContext";
+import { toast } from "react-toastify";
+function Header() {
+  const { theme, setTheme, store, lang } = useContext(CiteContext);
+  const notify = () => {
+    if (lang === "ENG") {
+      toast("O zaman hemen mustafabasaaar@gmail.com'a mail atıyorsun");
+    }
+    if (lang === "TÜRKÇE") {
+      toast("Then you immediately send an e-mail to mustafabasaaar@gmail.com");
+    }
+  };
+
+  return (
+    <header className="header-container">
+      <div
+        className={theme === "LIGHT" ? "header-symbol-dark" : "header-symbol"}
+      >
+        <div
+          className={
+            theme === "LIGHT" ? "header-symbol-text-dark" : "header-symbol-text"
+          }
+        >
+          A
+        </div>{" "}
+      </div>
+      <div className="header-links">
+        <div className="header-skill">{store.baslik.skills}</div>
+        <div className="header-projects">{store.baslik.projects}</div>
+        <div className="hire-button">
+          <button
+            onClick={notify}
+            className={theme === "LIGHT" ? "hire-button-dark" : ""}
+          >
+            {store.baslik.hireme}
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+export default Header;
